14fb4354ba72c53f599330f695497087d4dafe2f
TinaLorena <rosado.infodesign@gmail.com>
2025-04-26 17:11:39 -0400

    changes in deployment with GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/tina-final-project", // Add this line for GitHub Pages
   plugins: [react()],
 })
